424420de4d9bbf101d69f3c7a2aa6a204b27c276
8937361 <USUARIOS8937361@ltsp192.(none)>
2016-04-30 01:38:46 -0300

    makemakemake

---
+++ runcmd/config.mk
@@ -0,0 +1,13 @@
+PROJECT = runcmd
+
+CC = gcc
+CPP_FLAGS = -Wall --ansi --pedantic-errors -D_POSIX_C_SOURCE=200809L
+C_FLAGS =
+LD_FLAGS =
+MAKE = make
+AR = ar
+
+bin = 
+lib = libruncmd
+libruncmd_obj = runcmd.o
+libruncmd_h = runcmd.h
